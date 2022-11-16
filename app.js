67f95a17fe571a717116689de8693915e454158d
Gabriel Alvarez <jgabriel96@hotmail.com>
2022-11-15 20:02:17 -0600

    Innitial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+function moverPosicionRandom(elem){
+    elem.style.position = 'absolute';
+    elem.style.top = Math.random() * (window.innerHeight - elem.offsetHeight) + 'px'; /* Para que el "no" no se salga de la pantalla  */
+    elem.style.left = Math.random() * (window.innerWidth - elem.offsetWidth) + 'px'; /* Para que el "no" no se salga de la pantalla  */
+}
+
+let btnSi = document.getElementById("btn_si");
+let btnNo = document.getElementById("btn_no");
+let divModoSexo = document.getElementsByClassName("modo_sexo")[0];
+
+btnNo.addEventListener('mouseenter',function(e) {moverPosicionRandom(e.target)});
+
+btnSi.addEventListener('click',function(e){
+    alert('SABIA QUE DIRIAS QUE SI, CASEMONOS YA Y TENGAMOS HIJOS. TE AMO!!! ❤');
+
+    divModoSexo.style.display = 'block';
+    const cancion = new Audio('img\\modo_hot.mp3');
+    cancion.play();
+});
